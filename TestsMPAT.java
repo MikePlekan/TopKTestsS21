2a8b6ba525dd1a28c9acbf5ee6c28f1e2a667e51
MikePlekan <me26plek@siena.edu>
2021-03-27 22:34:51 -0400

    added and now uses custom class

--- TestsMPAT.java
+++ TestsMPAT.java
@@ -25,13 +25,13 @@ public class TestsMPAT
             System.out.println("Error.  Must enter long as command line parameter.");
             System.exit(-1);
         }
-        
+
         int[] list1=new int[input];
-        long[] list2=new long[input];
+        Cat[] list2=new Cat[input];
         TopKJDTRefSol top = new TopKJDTRefSol(5, Comparator.naturalOrder());
         for(int x =0;x<input;x++){
             list1[x]=rand.nextInt();
-            list2[x]=rand.nextLong();
+            list2[x]=new Cat(rand.nextDouble()*10.0);
         }
         for(int i:list1){
             top.add(i);
@@ -39,20 +39,46 @@ public class TestsMPAT
         Arrays.sort(list1);
         System.out.println("this is the last 5 of the list after being sorted");
         for(int x =input-5;x<input;x++){
-           System.out.println(list1[x]);
+            System.out.println(list1[x]);
         }
         System.out.println(top);
-        top.clear();
-        
-        
-        for(long i:list2){
+
+        top = new TopKJDTRefSol(5,new Cat(0));
+        for(Cat i:list2){
             top.add(i);
         }
         Arrays.sort(list2);
         System.out.println("this is the last 5 of the list after being sorted");
         for(int x =input-5;x<input;x++){
-           System.out.println(list2[x]);
+            System.out.println(list2[x]);
         }
         System.out.println(top);
+    }
+}
+class Cat implements Comparator<Cat>,Comparable<Cat>
+{
+    private double size;
+    public Cat(double size){
+        this.size=size;
+    }
+
+    public double getSize(){
+        return size;
+    }
+    @Override
+    public int compare(Cat a, Cat b) {
+        if (a.getSize() > b.getSize()) return 1;
+        if (a.getSize() < b.getSize()) return -1;
+        return 0;
+    }
+    @Override
+    public int compareTo(Cat b){
+        if (this.getSize() > b.getSize()) return 1;
+        if (this.getSize() < b.getSize()) return -1;
+        return 0;
+    }
+    @Override
+    public String toString(){
+        return "this cat is "+size+"kg";
     }
 }
